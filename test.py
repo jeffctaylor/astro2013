b031a1a6d01c516787b27318d2fc31a3d94d1822
Jeff Taylor <jeff.c.taylor@gmail.com>
2013-06-18 15:09:32 -0400

    Removed the naxis stuff. Now using astropy.io.fits instead of PyFITS

--- test.py
+++ test.py
@@ -6,33 +6,17 @@
 
 import astropy
 import glob
-import pyfits
+from astropy.io import fits
 
 # Grab all of the .fits and .fit files
 all_files = glob.glob('/Users/jeff.c.taylor/Dropbox/ASTROINFORMATICs/RAWdata/RAW/*.fit*')
 
 # Lists to store information
 all_wavelengths = []
-all_naxis1 = []
-all_naxis2 = []
 
 for i in all_files:
-        wavelength = pyfits.getval(i, 'WAVELENG')
+        wavelength = fits.getval(i, 'WAVELENG')
         all_wavelengths.append(wavelength)
-        naxis1 = pyfits.getval(i, 'NAXIS1')
-        all_naxis1.append(naxis1)
-        naxis2 = pyfits.getval(i, 'NAXIS2')
-        all_naxis2.append(naxis2)
 
 for i in all_wavelengths:
         print("Wavelength only: " + str(i))
-
-for i in all_naxis1:
-        print("NAXIS1 only: " + str(i))
-
-for i in all_naxis2:
-        print("NAXIS2 only: " + str(i))
-
-print("Maximums: ")
-print(str(max(all_naxis1)))
-print(str(max(all_naxis2)))
