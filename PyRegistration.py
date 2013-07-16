b239a9cd39e662500b0deeade85f51d58691da02
Jeff Taylor <jeff.c.taylor@gmail.com>
2013-07-16 12:00:24 -0400

    Added the code from PyResample.py. This is not working yet.

--- PyRegistration.py
+++ PyRegistration.py
@@ -478,7 +478,7 @@ def convolve_images_psf(images_with_headers):
         pyfits.writeto('science_image_convolved_4.fits',result4) 
 
 def convolve_images(images_with_headers):
-    print("Convolving images (currently being implemented)")
+    print("Convolving images")
     fwhm_input = get_fwhm_value(images_with_headers)
     print("fwhm_input = " + `fwhm_input`)
 
@@ -524,7 +524,57 @@ def convolve_images(images_with_headers):
 
 # Function: resample_images(images_with_headers)
 def resample_images(images_with_headers):
-    print("Resampling images (not implemented yet)")
+    print("Resampling images (currently being implemented)")
+
+    # First we create an artificial fits image, 
+    # The difference with the registration step is that the artificial image is now created only once, and it is common for all the input_images_convolved (or imput_images_gaussian_convolved)
+    # unlearn some iraf tasks
+    #
+    iraf.unlearn('mkpattern')
+    #
+    #create a fake image "grid_final_resample.fits", to which we will register all fits images
+    #
+    fwhm_input = get_fwhm_value(images_with_headers)
+    nyquist_sampling_rate = 3.3
+    # parameter1 & parameter2 depend on the "fwhm" of the convolution step, and following the Nyquist sampling rate. 
+    parameter1 = phys_size / (fwhm_input / nyquist_sampling_rate) 
+    parameter2 = parameter1
+    artdata.mkpattern(input="grid_final_resample.fits", output="grid_final_resample.fits", pattern="constant", pixtype="double", ndim=2, ncols=parameter1, nlines=parameter2)
+
+    for i in range(0, len(images_with_headers)):
+
+        native_pixelscale = get_native_pixelscale(images_with_headers[i][1], get_instrument(images_with_headers[i][1]))
+        print("Native pixel scale: " + `native_pixelscale`)
+        print("Instrument: " + `get_instrument(images_with_headers[i][1])`)
+        lngref_input = images_with_headers[i][1]['CRVAL1']
+        latref_input = images_with_headers[i][1]['CRVAL2']
+
+        original_filename = os.path.basename(images_with_headers[i][2])
+        original_directory = os.path.dirname(images_with_headers[i][2])
+        new_directory = original_directory + "/resampled/"
+        resampled_filename = new_directory + original_filename  + "_resampled.fits"
+        input_directory = original_directory + "/convolved/"
+        input_filename = input_directory + original_filename  + "_convolved.fits"
+        print("Resampled filename: " + resampled_filename)
+        print("Input filename: " + input_filename)
+        if not os.path.exists(new_directory):
+            os.makedirs(new_directory)
+
+        #Then, we tag the desired WCS in this fake image:
+        # unlearn some iraf tasks
+        iraf.unlearn('ccsetwcs')
+
+        #tag the desired WCS in the fake image "apixel.fits"
+        iraf.ccsetwcs(images="grid_final_resample.fits", database="", solution="", xref=ncols/2, yref=nlines/2, xmag=fwhm/nyquist_sampling_rate, ymag=fwhm/nyquist_sampling_rate, xrotati=0.,yrotati=0.,lngref=same_as_we_put_in_the_registration, latref=same_as_we_put_in_the_registration, lngunit="hours", latunit="degrees", transpo="no", project="tan", coosyst="j2000", update="yes", pixsyst="logical", verbose="yes")
+
+        # Then, register the fits file of interest to the WCS of the fake fits file
+        # unlearn some iraf tasks
+        #
+        iraf.unlearn('wregister')
+        #register the sciense fits image
+        #
+        #iraf.wregister(input="input_image_convolved.fits", reference="grid_final_resample.fits", output="input_image_final.fits", fluxconserve="yes")
+        #
 
 # Function: output_seds(images_with_headers)
 def output_seds(images_with_headers):
